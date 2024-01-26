70542c8520f50cfbc7db7f6db1d0fee7a0595211
Madhukar yerraguntla <mycroft@Madhukars-MacBook-Air.local>
2024-01-27 01:31:48 +0530

    parsing is to be made like lexing, recursive descent

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,7 +1,14 @@
 module Parser where
     
-    import TokenTypes
+    import TokenTypes ( Token )
+    import Error (ErrInfo)
+    import Expr
 
-    newtype ParserState = ParserState {
-        tokens:: [Token]
-    }
+    data ParserState = ParserState {
+        tokens:: [Token],
+        index :: Int
+    } deriving (Show, Eq)
+
+    newtype Parser a = Parser {runParse :: ParserState -> Either ErrInfo (a, ParserState)}
+
+    
